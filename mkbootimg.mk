9c7727cd1b346899bd27e12b4e790ed8d387a312
Tim Zimmermann <tim@linux4.de>
2020-10-18 21:37:07 +0200

    gta3xl: Build and flash a disabled vbmeta image
    A combination of --flags 0 and --algorithm NONE
    disables avb verification through bootloader
    while --flags 2 and --set_hastree_disabled_flag
    lead to reboot to download mode with vbmeta error
    
    Change-Id: Ia7860243c1d14cfc8ac54b9801802866d88f47a1

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -11,10 +11,16 @@ DTB_CFG    := $(COMMON_PATH)/configs/kernel/exynos7885.cfg
 
 INSTALLED_DTBIMAGE_TARGET := $(PRODUCT_OUT)/dtb.img
 
-$(INSTALLED_DTBIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(MKDTIMG)
+$(INSTALLED_DTBIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(MKDTIMG) $(AVBTOOL)
 	$(hide) echo "Building dtb.img"
 	$(hide) $(MKDTIMG) cfg_create $@ $(DTB_CFG) -d $(DTB_DIR)
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_DTBIMAGE_PARTITION_SIZE),raw)
+	$(hide) $(AVBTOOL) add_hash_footer \
+	  --image $@ \
+	  --partition_size $(BOARD_DTBIMG_PARTITION_SIZE) \
+	  --partition_name dtb \
+	  --algorithm $(BOARD_AVB_ALGORITHM) \
+	  --key $(BOARD_AVB_KEY_PATH)
 
 .PHONY: dtbimage
 dtbimage: $(INSTALLED_DTBIMAGE_TARGET)
