2a9af6d00ba978687b1ffcf7412d461cc786f15d
Giovanni Ricca <giovanniricca@proton.me>
2022-11-19 17:46:33 +0100

    gta3xl: Define kernel defconfig
    Change-Id: I432335482eb41e1bfbaa9b2b73be8cb342b26fb9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,5 +9,8 @@ DEVICE_PATH := device/samsung/gta3xl
 # Inherit from gta3xl-common
 include device/samsung/gta3xl-common/BoardConfigCommon.mk
 
+# Kernel
+TARGET_KERNEL_CONFIG := exynos7885-gta3xl_defconfig
+
 # Include proprietary files
 -include vendor/samsung/gta3xl/BoardConfigVendor.mk
