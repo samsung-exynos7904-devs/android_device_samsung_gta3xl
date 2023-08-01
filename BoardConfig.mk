cdb0d83d6a5a192304d6b75be7d91686ab568b0c
Giovanni Ricca <giovanniricca@proton.me>
2023-08-01 22:55:13 +0200

    gta3xl: Add architecture flags
    Change-Id: I90872d4341fa95085de39ddc11f62bcf299fe4e0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,15 @@
 
 DEVICE_PATH := device/samsung/gta3xl
 
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
+
+TARGET_USES_64_BIT_BINDER := true
+
 # Inherit the proprietary files
 include vendor/samsung/gta3xl/BoardConfigVendor.mk
